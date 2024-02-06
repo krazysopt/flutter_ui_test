bc02e6aff2131d16dae47a4fc63a58a8a9ab5413
atmodesm <kai@flutterdevelopers.com>
2024-02-06 04:45:45 -0500

    change title

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter UI Test',
+      title: 'Flutter UI Test2',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
